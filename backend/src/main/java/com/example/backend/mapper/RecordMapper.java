7926e5851e6f29033ddc70324847d7565d19e55c
lao nuo <1041796963@qq.com>
2023-08-16 14:41:40 +0800

    实现微服务,匹配系统(中)

---
+++ backend/src/main/java/com/example/backend/mapper/RecordMapper.java
@@ -0,0 +1,11 @@
+package com.example.backend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import com.example.backend.pojo.Record;
+
+@Mapper
+public interface RecordMapper extends BaseMapper<Record> {
+
+
+}
--- web/src/components/GameMap.vue
+++ web/src/components/GameMap.vue
@@ -17,7 +17,10 @@ export default {
         let canvas = ref(null);
 
         onMounted(() => {
+           store.commit(
+            "updateGameObject",
             new GameMap(canvas.value.getContext('2d'), parent.value,store)
+           );
         });
 
         return {
